23f16c361cf07a6a466580939d88c2b736447ed4
szktty <szk0115tty@gmail.com>
2022-12-02 19:40:49 +0900

    更新

--- app/resources/views/login.blade.php
+++ app/resources/views/login.blade.php
@@ -8,6 +8,10 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- オリジナル CSS -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
     <title>ログイン画面</title>
 </head>
 
