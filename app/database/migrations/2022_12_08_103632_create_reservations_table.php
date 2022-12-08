7aea2cde758070e54750c941992fb7db949366be
szktty <szk0115tty@gmail.com>
2022-12-08 20:27:43 +0900

    更新します

---
+++ app/database/migrations/2022_12_08_103632_create_reservations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->bigIncrements('user_id');
+            $table->bigIncrements('event_id');
+            $table->tinyint('judgment');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
--- app/routes/web.php
+++ app/routes/web.php
@@ -28,3 +28,7 @@ Route::get('/newlogin', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
