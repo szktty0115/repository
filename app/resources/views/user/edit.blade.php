5553db1e15f93203606dd9c072d80b972432b120
szktty <szk0115tty@gmail.com>
2022-12-16 00:24:32 +0900

    更新

--- app/resources/views/user/edit.blade.php
+++ app/resources/views/user/edit.blade.php
@@ -17,41 +17,55 @@
 
 <body class="text-center bg-light">
     <h1 class="h10 my-10">一般ユーザー編集</h1>
-
-    <form class="border rounded bg-white form-login">
-        <div class="form-group">
-            <p>氏名</p>
-            <input type="name" class="form-control my3" placeholder="氏名">
+    @extends('layouts.app') @section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">ユーザ編集</div>
+                    <div class="card-body">
+                        <!-- 重要な箇所ここから -->
+                        <form action="" method="post">
+                            @csrf
+                            <p>ID: {{ $user->id }}</p>
+                            <input type="hidden" name="id" value="{{ $user->id }}" />
+                            <p>名前</p>
+                            <input type="text" name="name" value="{{ $user->name }}" />
+                            <p>メール</p>
+                            <input type="text" name="email" value="{{ $user->email }}" /><br />
+                            <input type="submit" value="更新" />
+                        </form>
+                        <!-- 重要な箇所ここまで -->
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="form-group">
-            <p>生年月日</p>
-            <input type="" class="form-control my3" placeholder="生年月日">
-        </div>
-        <div class="form-group">
-            <p>電話番号</p>
-            <input type="tel" class="form-control my3" placeholder="電話版号">
-        </div>
-        <div class="form-group pt-3">
-            <p>メールアドレス</p>
-            <input type="email" class="form-control my-3" placeholder="メールアドレス">
-        </div>
-        <div class="my-3 form-check">
-            <button type="submit" class="btn btn-primary">更新</button>
-        </div>
-    </form>
-
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
+    </div>
+    @endsection
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
 </body>
 
 </html>
+
+<!-- <form class="border rounded bg-white form-login">
+    <div class="form-group">
+        <p>氏名</p>
+        <input type="name" class="form-control my3" placeholder="お名前">
+    </div>
+    <div class="form-group">
+        <p>生年月日</p>
+        <input type="" class="form-control my3" placeholder="生年月日">
+    </div>
+    <div class="form-group">
+        <p>電話番号</p>
+        <input type="tel" class="form-control my3" placeholder="電話版号">
+    </div>
+    <div class="form-group pt-3">
+        <p>メールアドレス</p>
+        <input type="email" class="form-control my-3" placeholder="メールアドレス">
+    </div>
+    <div class="my-3 form-check">
+        <button type="submit" class="btn btn-primary">更新</button>
+    </div>
+</form> -->
